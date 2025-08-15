0d571f186c33732efc5140468bae9b490c3fb88d
Apollo-Dionysus <9439680+montycpython@users.noreply.github.com>
2025-08-14 20:08:21 -0400

    Update makeFancyString.py
    Found another solution...

--- makeFancyString.py
+++ makeFancyString.py
@@ -22,3 +22,20 @@ class Solution2: #336ms, 19.65MB; time: O(n) linear, space: O(n) linear
               continue
           res.append(char)
       return "".join(res)
+
+class Solution3: #239ms, 19.58Mb
+    def makeFancyString(self, s: str) -> str:
+        ans = []
+        last = s[0]
+        ans.append(last)
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == last:
+                count += 1
+                if count < 3:
+                    ans.append(s[i])
+            else:
+                last = s[i]
+                count = 1
+                ans.append(s[i])
+        return "".join(ans)
